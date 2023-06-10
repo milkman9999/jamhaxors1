7e9b9bd56f700e3d09875d386968d65c0ec86c27
milkman9999 <matthew.zhu3s@gmail.com>
2023-06-10 17:45:04 -0400

    post request handling

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def initialize_the_boy():
     return render_template("index.html")
 
-@app.route("/")
-def hello_world():
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting inputs from html form index
+       age = request.form.get("age")
+       weight = request.form.get("weight")
+       sex = request.form.get("sex")
+       days = request.form.get("days")
+       time = request.form.get("time")
+       return "age ="+age+", weight ="+weight+", sex ="+sex+", days ="+days+", time ="+time
     return render_template("index.html")
-
+ 
+if __name__=='__main__':
+   app.run()
 
 # run
 #   flask --app server run 
