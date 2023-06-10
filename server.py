7b890e239b88ca8f3efa64b7d57d0470b35506a8
milkman9999 <matthew.zhu3s@gmail.com>
2023-06-10 16:56:36 -0400

    server py and index html changes

--- server.py
+++ server.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
 
-#run flask --app server run to start the website
+
+# run
+#   flask --app server run 
+# to start the website
