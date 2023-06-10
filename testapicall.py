935e7e7f7fefb798d6869d81dd3d2f93f875de5d
milkman9999 <matthew.zhu3s@gmail.com>
2023-06-10 09:04:29 -0400

    api_interact folder created, infrastructure for future api calling

--- testapicall.py
+++
@@ -1,9 +0,0 @@
-import openai
-from config import API_KEY
-openai.api_key= API_KEY
-
-prompt = "Say this is a test"
-
-response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=10)
-
-print(response)
