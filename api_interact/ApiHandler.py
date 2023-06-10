a0f7d771a640f48562dd13060d3c2ccaf25ec978
milkman9999 <matthew.zhu3s@gmail.com>
2023-06-10 11:47:41 -0400

    can now talk with the AI

--- api_interact/ApiHandler.py
+++ api_interact/ApiHandler.py
@@ -8,5 +8,3 @@ def gen_from_prompt(privprompt, temperature, max_tokens):
     prompt = privprompt    
     response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=10)
     return response.choices[0]['text']
-
-print(gen_from_prompt("hello ai"))
