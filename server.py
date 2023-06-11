6166a528cbda7302a7f5390d5524880188012bc8
soappeea <124820419+soappeea@users.noreply.github.com>
2023-06-10 20:18:35 -0400

    cry

--- server.py
+++ server.py
@@ -1,25 +1,33 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+import sys
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/main/")
 def initialize_the_boy():
+    print('initailize the boy')
     return render_template("index.html")
 
-@app.route('/', methods =["GET", "POST"])
+@app.route('/form/', methods =["GET", "POST"])
 def gfg():
+    print('in gfg', file=sys.stdout)
     if request.method == "POST":
+       print('in post req', file=sys.stdouts)
        # getting inputs from html form index
        age = request.form.get("age")
        weight = request.form.get("weight")
        sex = request.form.get("sex")
        days = request.form.get("days")
        time = request.form.get("time")
-       return "age ="+age+", weight ="+weight+", sex ="+sex+", days ="+days+", time ="+time
-    return render_template("index.html")
+       print(age)
+       print(weight)
+       print(time)
+       return jsonify({'age':age}, {'weight':weight}, {'sex':sex}, {'days':days}, {'time':time})
+    #    return "your info: age ="+age+", weight ="+weight+", sex ="+sex+", days ="+days+", time ="+time
  
 if __name__=='__main__':
-   app.run()
+   print('running app...', file=sys.stdout)
+   app.run(debug = True)
 
 # run
 #   flask --app server run 
