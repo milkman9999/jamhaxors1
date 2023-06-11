abce1971366d3c245849a40a5ad4aefe44e4e72f
milkman9999 <matthew.zhu3s@gmail.com>
2023-06-11 00:35:10 -0400

    more index/server adjustments

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import sys
 
 app = Flask(__name__)
 
-@app.route("/main/")
+@app.route("/main")
 def initialize_the_boy():
     print('initailize the boy')
     return render_template("index.html")
@@ -11,6 +11,18 @@ def initialize_the_boy():
 @app.route('/form/', methods =["GET", "POST"])
 def gfg():
     print('in gfg')
+    if request.method == "GET":
+       print('in get req')
+       # getting inputs from html form index
+       age = request.form.get("age")
+       weight = request.form.get("weight")
+       sex = request.form.get("sex")
+       days = request.form.get("days")
+       time = request.form.get("time")
+       print(age)
+       print(weight)
+       print(time)
+       return jsonify({'age':age, 'weight':weight, 'sex':sex, 'days':days, 'time':time})
     if request.method == "POST":
        print('in post req')
        # getting inputs from html form index
